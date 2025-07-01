1bdf70b1c30471345054f90f00bb3747428b05c1
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-07-02 00:32:54 +0900

    3D update

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target; // Follow 대상
+    public Vector3 offset; // 대상과의 거리
+
+    void Update()
+    {
+        transform.position = target.position + offset; // 대상의 위치에 오프셋을 더하여 위치 설정
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    float hAxis;
+    float vAxis;
+    bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
+
+    Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody rb;
+    Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec; // 회피 중에는 회피 방향으로 이동
+        //trasnform 이동은 fixedupdate 에서 동작하지 않으면 물리엔진 충돌 발생가능성 존재, 벽뚫기 등
+        transform.position += moveVec * Time.deltaTime * speed * (wDown ? 0.3f : 1f);
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)
+        {
+            rb.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f); // 0.5초 후 회피 종료
+        }
+    }
+
+    void DodgeOut()
+    {
+        isDodge = false;
+        speed *= 0.5f; // 회피 후 속도 감소
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false; // 바닥에 닿으면 점프 가능
+        }
+    }
+}
